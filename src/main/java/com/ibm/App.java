6fd3991aa15f7d6dae45894f844140c29eb936f7
Hicham Layadi <hh@rr.com>
2019-09-19 11:20:48 +0100

    git ignore

---
+++ src/main/java/com/ibm/App.java
@@ -0,0 +1,18 @@
+package com.ibm;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+
+    public int getAge(){
+        return 10;
+    }
+
+}
