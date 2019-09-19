f2e1f06ec004886336b74fd994b143d34e086b6f
Hicham layadi <mlayadi@nestle.com>
2019-09-19 19:31:41 +0200

    changed message

--- src/main/java/com/ibm/App.java
+++ src/main/java/com/ibm/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Peoples! o" );
+        System.out.println( "Hello Peoples! " );
     }
 
     public int getAge(){
