b0820d5c0ccdc29123831872aeb94f78357c2d8d
lhechma <hicham.layadi@gmail.com>
2019-09-20 12:19:28 +0200

    Update App.java

--- src/main/java/com/ibm/App.java
+++ src/main/java/com/ibm/App.java
@@ -6,11 +6,6 @@ package com.ibm;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello Peoples! " );
-    }
-
     public int getAge(){
         return 12;
     }
